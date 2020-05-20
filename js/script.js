3cae333388db6a1bcf179d8663c94c51c3e7c897
Neto25 <euneto2501@gmail.com>
2020-05-20 17:54:50 -0300

    adicionando movimentação a snake

--- js/script.js
+++ js/script.js
@@ -20,6 +20,15 @@ function criarSnake(){
     }
 }
 
+document.addEventListener("keydown", update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
     criarBG();
     criarSnake();
@@ -28,7 +37,7 @@ function iniciarJogo(){
     let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
-    if(direction == "left") snakeX += box;
+    if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
@@ -43,3 +52,4 @@ function iniciarJogo(){
 }
 
 let jogo = setInterval(iniciarJogo, 100);
+
