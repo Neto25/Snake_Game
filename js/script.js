e3c186c3233f8e793ffd493873d05205a1cd0708
Neto25 <euneto2501@gmail.com>
2020-05-20 19:00:02 -0300

    adicionado o choque com o corpo da snake, assim surgindo o alerta; mudança nos nomes das funções

--- js/script.js
+++ js/script.js
@@ -12,12 +12,12 @@ let food = {
     y: Math.floor(Math.random() * 15 + 1) * box
 }
 
-function criarBG(){
+function createBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarSnake(){
+function createSnake(){
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
@@ -31,21 +31,28 @@ function drawfood(){
 
 document.addEventListener("keydown", update);
 
-function update (event){
+function update(event){
     if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
-function iniciarJogo(){
+function startGame(){
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
-    criarBG();
-    criarSnake();
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert("perdeu");
+        }
+    }
+
+    createBG();
+    createSnake();
     drawfood();
 
     let snakeX = snake[0].x;
@@ -71,5 +78,5 @@ function iniciarJogo(){
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let game = setInterval(startGame, 100);
 
